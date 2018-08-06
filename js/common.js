fe5277d2b9031e8b3659ed2b3f6a6838597de8c7
Thái Nhật Minh <t.n.minh1997@gmail.com>
2018-08-07 00:50:38 +0700

    - fixed wrong month

--- js/common.js
+++ js/common.js
@@ -12,7 +12,7 @@ module.exports =
     GetDate: function(slash = '/')
     {
         var d = new Date();
-        var result = d.getDate() + slash + d.getMonth() + slash + d.getFullYear();
+        var result = d.getDate() + slash + (d.getMonth()+1) + slash + d.getFullYear();
         console.log(result);
         return result;
     }
