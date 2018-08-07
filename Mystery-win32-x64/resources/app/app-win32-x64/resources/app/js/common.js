a0d4acaf6c57b3e0a5fb7e8c23be349b06364137
ThaiNhatMinh <t.n.minh1997@gmail.com>
2018-08-07 16:56:14 +0700

    + Build to Win32 App

---
+++ Mystery-win32-x64/resources/app/app-win32-x64/resources/app/js/common.js
@@ -0,0 +1,19 @@
+function _GetDate(slash = '/')
+{
+    var d = new Date();
+    var result = d.getDate() + slash + d.getMonth() + slash + d.getFullYear();
+    console.log(result);
+    return result;
+}
+
+
+module.exports = 
+{
+    GetDate: function(slash = '/')
+    {
+        var d = new Date();
+        var result = d.getDate() + slash + d.getMonth() + slash + d.getFullYear();
+        console.log(result);
+        return result;
+    }
+};
